b3a4d71b459808ae5fb93eaec561f41b04dac245
Shota Matsumoto <shota.matsumoto@lifematics.co.jp>
2024-01-12 17:54:05 +0900

    Draw pathway by custom widget

--- src/qp.py
+++ src/qp.py
@@ -4,37 +4,38 @@ import json
 import os
 import pandas as pd
 from IPython.display import display, HTML, clear_output
-from ipywidgets import widgets
+from traitlets import Unicode, Bool, validate, TraitError
+from ipywidgets import DOMWidget, register, interact, interactive, widgets
 from src.gpml_parser import GpmlParser
-from jinja2 import Environment, FileSystemLoader
  
     
-class GpmlD3Visualizer:
-    def __init__(self, gpml):
-        self.pathway_data = GpmlParser(gpml).data
-        self.template = Environment(loader=FileSystemLoader("templates", encoding='utf-8')).get_template('pathway_by_d3.html')
 
-    def show(self, width=800, height=1000):
-        html = self.template.render({"pathway_data": { "nodes": self.pathway_data["nodes"], 
-                                                 "links": self.pathway_data["interactions"],
-                                                   "shapes": self.pathway_data["shapes"],
-                                                   "groups": self.pathway_data["groups"],
-                                                    "pathway": self.pathway_data["pathway"] },
-                                     "width": width,
-                                     "height": height,
-                                     })
 
-        return html
+@register
+class GpmlD3VisualizerWidget(DOMWidget):
+    _view_name = Unicode('PathwayD3View').tag(sync=True)
+    _view_module = Unicode('pathway_d3_view_widget').tag(sync=True)
+    _view_module_version = Unicode('0.1.0').tag(sync=True)
+
+    value = Unicode('', help="").tag(sync=True)
+    width = Unicode('', help="").tag(sync=True)
+    pathway_data = Unicode('', help="").tag(sync=True)
     
-        
+    def __init__(self, gpml):
+        super().__init__()
+        self.pathway_data = json.dumps(GpmlParser(gpml).data)
 
-class GpmlD3VisualizerWidget:
+
+
+class GpmlD3Visualizer:
     def __init__(self, gpml_dir_path, gene_data_path):
         self.gpml_dir_path = gpml_dir_path
         self.gene_data = pd.read_table(gene_data_path)
         self.visualizer = None
         self.selected_gpml_file = None
-
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        self.d3_pathway_html = open(os.path.join(current_dir, './templates/pathway_by_d3.html'), 'r').read()
+    
 
     def show(self):
         gpml_files = glob.glob("{}/*.gpml".format(self.gpml_dir_path))
@@ -49,29 +50,38 @@ class GpmlD3VisualizerWidget:
             style={'width': 'max-content'},
         )
 
-        w = widgets.Box(
+        self.visualizer_widget = GpmlD3VisualizerWidget(gpml=os.path.join(self.gpml_dir_path, self.selected_gpml_file))
+
+        self.widgets = widgets.Box(
             [
                 widgets.Label(value='Select GPML file:'), 
-                dropdown
+                dropdown,
+                self.visualizer_widget
             ]       
         )
 
         def visualize_gpml(gpml_file):
-            self.visualizer = GpmlD3Visualizer(gpml=os.path.join(self.gpml_dir_path, gpml_file))
+            self.visualizer_widget = GpmlD3VisualizerWidget(gpml=os.path.join(self.gpml_dir_path, gpml_file))
             clear_output(wait=True)
-            display(w)
-            display(HTML(self.visualizer.show()))
+            self.widgets = widgets.Box(
+                [
+                    widgets.Label(value='Select GPML file:'), 
+                    dropdown,
+                    self.visualizer_widget
+                ]       
+            )
+            display(HTML(self.d3_pathway_html))
+            display(self.widgets)
 
         def on_change(change):
             if change['type'] == 'change' and change['name'] == 'value':
                 self.selected_gpml_file = change['new']
                 visualize_gpml(self.selected_gpml_file)
 
-        visualize_gpml(self.selected_gpml_file)
-
         dropdown.observe(on_change)
 
-        return display(w)
+        display(HTML(self.d3_pathway_html))
+        return display(self.widgets)
 
     
     def table(self, gid:str):
