d5fdae5543891b6cd18c10267a9e34de9f6aaf3f
Shota Matsumoto <matsusho070@gmail.com>
2024-03-18 14:55:09 +0900

    Add itables library

--- src/qp.py
+++ src/qp.py
@@ -3,11 +3,13 @@ import glob
 import json
 import os
 import polars as pl
+import pandas as pd
 from IPython.display import display, HTML, clear_output
 from traitlets import Unicode, Bool, validate, TraitError
 from ipywidgets import DOMWidget, register, interact, interactive, widgets
 from src.gpml_parser import GpmlParser
- 
+from itables import show
+
     
 
 
@@ -20,9 +22,10 @@ class PathwayD3VisualizerWidget(DOMWidget):
     value = Unicode('', help="").tag(sync=True)
     pathway_data = Unicode('', help="").tag(sync=True)
     
-    def __init__(self, pathway_data):
+    def __init__(self, pathway_data, value):
         super().__init__()
         self.pathway_data = pathway_data
+        self.value = value
 
 
 
@@ -34,6 +37,7 @@ class GpmlD3Visualizer:
         self.filter_key = filter_key
         self.visualizer = None
         self.selected_gpml_file = None
+        self.gid = None
     
 
     def show(self):
@@ -48,36 +52,48 @@ class GpmlD3Visualizer:
             value=self.selected_gpml_file,
         )
 
-        def display_gene_data(gid:str):
-            d = self.gene_data
-            # xref_idでフィルターするように変更（2024/1/29oec）
-            if gid:
-                # データテーブルでフィルターしたい属性を指定
-                # if gid is integer like string, convert it to integer
-                if gid.isdigit():
-                    gid = int(gid)
-                d = d.filter(pl.col(self.filter_key) == gid)
-                print("Xref ID {}:".format(gid))
-            else:
-                print("Gene data:")
-            self.selected_gene_data = d
-            display(d)
-
         def visualize(gpml_file:str):
             self.visualizer_widget.pathway_data = json.dumps(GpmlParser(os.path.join(self.gpml_dir_path, gpml_file)).data)
             display(self.visualizer_widget)
 
-        self.visualizer_widget = PathwayD3VisualizerWidget(pathway_data=json.dumps(GpmlParser(os.path.join(self.gpml_dir_path, self.selected_gpml_file)).data))
-        self.interactive_visualizer = widgets.interactive_output(visualize, {'gpml_file': dropdown})
-        self.dataframe_widget = widgets.interactive_output(display_gene_data, {'gid': self.visualizer_widget})
 
+        # interactive_output使用時、itablesが描画直後だけ表示されない問題があるため、タイマーをかけてすぐに再描画するようにする
+        initial_dummy_value = "　"
+        self.visualizer_widget = PathwayD3VisualizerWidget(pathway_data=json.dumps(GpmlParser(os.path.join(self.gpml_dir_path, self.selected_gpml_file)).data), value=initial_dummy_value)
+        from threading import Timer
+        def redraw():
+            self.visualizer_widget.value = ""
+        timer = Timer(0.2, redraw, ())
+        timer.start()
+
+        self.interactive_visualizer = widgets.interactive_output(visualize, {'gpml_file': dropdown})
+
+        def display_gene_data(gid:str):
+            d = self.gene_data
+            self.gid = gid
+            # xref_idでフィルターするように変更（2024/1/29oec）
+            if gid:
+                # データテーブルでフィルターしたい属性を指定
+                d = d.filter(pl.col(self.filter_key).cast(pl.datatypes.Utf8) == str(gid))
+                if d.shape[0] == 0:
+                    print("No data found for Xref ID {}".format(gid))
+                    return
+                print("Xref ID {}:".format(gid))
+            else:
+                print("Gene data:")
+            self.selected_gene_data = d
+            show(self.selected_gene_data, classes="display compact")
+
+        dataframe_output = widgets.interactive_output(display_gene_data, {"gid": self.visualizer_widget})
+        
         self.widgets = widgets.VBox( 
             [
                 widgets.HBox([widgets.Label(value='Select GPML file:'), 
                     dropdown]),
                 self.interactive_visualizer,      
-                self.dataframe_widget      
+                dataframe_output      
             ]
         )
 
-        return display(self.widgets)
+
+        display(self.widgets)
